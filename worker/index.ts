84ba065f94c8f0d597549018c44df4d5d05a8cee
LeoSzn12 <ankitpatelinc@gmail.com>
2026-07-25 22:43:52 -0700

    build: add Sites-compatible vinext output

---
+++ worker/index.ts
@@ -0,0 +1,9 @@
+import handler from "vinext/server/app-router-entry";
+
+const worker = {
+  async fetch(request: Request, env: unknown, ctx: ExecutionContext): Promise<Response> {
+    return handler.fetch(request, env, ctx);
+  },
+};
+
+export default worker;
